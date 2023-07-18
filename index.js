194a4163bf614396274fc501aeaaeb92c85608e7
ebenpack <ebenpackwood@gmail.com>
2023-07-18 00:12:54 -0400

    Refactor a bit

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-let clicked = null;
-const scale = 1.5;
-const canvasContainer = document.getElementById("");
-const canvas = document.getElementById("canvas");
-const hitboxes = {};
-canvas.addEventListener("click", (e) => {
-  const canvasRect = canvas.getBoundingClientRect();
-  const x = Math.floor(e.clientX - canvasRect.left);
-  const y = Math.floor(e.clientY - canvasRect.top);
+const main = async () => {
+  const scale = 1.5;
+  const canvasContainer = document.getElementById("shark-game");
+  const hitboxes = {};
 
-  for (const [id, vals] of Object.entries(hitboxes)) {
-    if (vals[`${x}-${y}`]) {
-      clicked = 21 - parseInt(id.split("-")[1], 10);
-      drawFrame();
-      break;
-    }
-  }
-});
-const drawFrame = async () => {
   // TODO: DRAWFRAME IS F'N SLOW
   const maxWidth = 390;
   const maxHeight = 404;
+  let actualWidth = 0;
+  let actualHeight = 0;
 
-  const images = [
+  let images = [
     {
       id: "background",
       src: "background.png",
@@ -37,156 +25,171 @@ const drawFrame = async () => {
       top: 0,
     },
     { id: "shark", src: "shark.png", width: 392, left: 30, top: 30 },
+    ...[...Array(20)].map((_, index) => {
+      const i = index + 1;
+      return {
+        id: `tooth-${i}`,
+        src: i < 10 ? `tooth_0${i}.png` : `tooth_${i}.png`,
+        width: 392,
+        left: 30,
+        top: 30,
+      };
+    }),
   ];
 
-  for (i = 1; i <= 20; i++) {
-    const src = i < 10 ? `tooth_0${i}.png` : `tooth_${i}.png`;
-    images.push({
-      id: `tooth-${i}`,
-      src,
-      width: 392,
-      left: 30,
-      top: 30,
-      // TODO: THESE ARE COUNTING FROM DIFF DIRECTIONS THIS IS DUMB
-      tint: 21 - i === clicked ? "#F00" : null,
-    });
-  }
+  // Scale everything
+  images = images.map(({ width, height, left, top, ...rest }) => ({
+    width: Math.floor(width * scale),
+    height: Math.floor((maxHeight / maxWidth) * width * scale),
+    left: Math.floor(left * scale),
+    top: Math.floor(top * scale),
+    ...rest,
+  }));
 
   // find the largest dimesntions and set the canvas width/height to that
   images.forEach(({ width, height }) => {
-    const scaledWidth = Math.floor(width * scale);
-    const scaledHeight = Math.floor((maxHeight / maxWidth) * width * scale);
-    if (canvas.width < scaledWidth) {
-      canvas.width = scaledWidth;
+    if (actualWidth < width) {
+      actualWidth = width;
     }
-    if (canvas.height < scaledHeight) {
-      canvas.height = scaledHeight;
+    if (actualHeight < height) {
+      actualHeight = height;
     }
   });
+  canvasContainer.style = `position: relative; width: ${actualWidth}px; height: ${actualHeight}`;
 
-  const backBuffer = window.OffscreenCanvas
-    ? new OffscreenCanvas(canvas.width, canvas.height)
-    : document.createElement("canvas");
-  backBuffer.width = canvas.width;
-  backBuffer.height = canvas.height;
-  const backBufferCtx = backBuffer.getContext("2d");
-
-  backBufferCtx.imageSmoothingEnabled = false;
-
-  const buffer = document.createElement("canvas");
-  buffer.width = canvas.width;
-  buffer.height = canvas.height;
-
-  const btx = buffer.getContext("2d");
-  btx.imageSmoothingEnabled = false;
-
-  const container = document.createElement("div");
-
-  const makeImg = async ({ id, src, width, left, top, tint = null }) => {
+  const makeImg = async (image) => {
+    const { id, src, width, height, left, top } = image;
+    const container = document.createElement("div");
     const img = document.createElement("img");
     img.src = `images/${src}`;
     container.appendChild(img);
     return new Promise((resolve) => {
-      const scaledLeft = Math.floor(left * scale);
-      const scaledTop = Math.floor(top * scale);
-      const scaledWidth = Math.floor(width * scale);
-      const scaledHeight = Math.floor((maxHeight / maxWidth) * width * scale);
       img.addEventListener("load", (e) => {
-        if (tint) {
-          btx.clearRect(0, 0, buffer.width, buffer.height);
-          btx.drawImage(img, scaledLeft, scaledTop, scaledWidth, scaledHeight);
+        // if (tint) {
+        //   btx.clearRect(0, 0, buffer.width, buffer.height);
+        //   btx.drawImage(img, left, top, width, height);
 
-          // Now we'll multiply a rectangle of your chosen color
-          btx.fillStyle = tint;
-          btx.globalCompositeOperation = "multiply";
-          btx.fillRect(0, 0, buffer.width, buffer.height);
+        //   // Now we'll multiply a rectangle of your chosen color
+        //   btx.fillStyle = tint;
+        //   btx.globalCompositeOperation = "multiply";
+        //   btx.fillRect(0, 0, buffer.width, buffer.height);
 
-          btx.globalAlpha = 0.5;
-          btx.globalCompositeOperation = "destination-in";
-          btx.drawImage(img, scaledLeft, scaledTop, scaledWidth, scaledHeight);
-          backBufferCtx.drawImage(
-            img,
-            scaledLeft,
-            scaledTop,
-            scaledWidth,
-            scaledHeight
-          );
-          backBufferCtx.drawImage(buffer, 0, 0, buffer.width, buffer.height);
-        } else {
-          if (id.startsWith("tooth")) {
-            hitboxes[id] = {};
-            const pixelFinder = document.createElement("canvas");
-            pixelFinder.width = canvas.width;
-            pixelFinder.height = canvas.height;
-            const pxlCtx = pixelFinder.getContext("2d");
-            pxlCtx.drawImage(
-              img,
-              scaledLeft,
-              scaledTop,
-              scaledWidth,
-              scaledHeight
-            );
-            const imgd = pxlCtx.getImageData(0, 0, canvas.width, canvas.height);
-            var pix = imgd.data;
-            for (let i = 0; i < pix.length; i += 4) {
-              const x = Math.round((i / 4) % canvas.width);
-              const y = Math.round(i / 4 / canvas.height);
-              const alpha = pix[i + 3];
-              if (alpha > 20) {
-                hitboxes[id][`${x}-${y}`] = true;
-              }
+        //   btx.globalAlpha = 0.5;
+        //   btx.globalCompositeOperation = "destination-in";
+        //   btx.drawImage(img, left, top, width, height);
+        //   backBufferCtx.drawImage(img, left, top, width, height);
+        //   backBufferCtx.drawImage(buffer, 0, 0, buffer.width, buffer.height);
+        // } else {
+        if (id.startsWith("tooth") && !hitboxes[id]) {
+          hitboxes[id] = {};
+          const pixelFinder = document.createElement("canvas");
+          pixelFinder.width = actualWidth;
+          pixelFinder.height = actualHeight;
+          const pxlCtx = pixelFinder.getContext("2d");
+          pxlCtx.drawImage(img, left, top, width, height);
+          const imgd = pxlCtx.getImageData(0, 0, actualWidth, actualHeight);
+          var pix = imgd.data;
+          for (let i = 0; i < pix.length; i += 4) {
+            const x = Math.round((i / 4) % actualWidth);
+            const y = Math.round(i / 4 / actualHeight);
+            const alpha = pix[i + 3];
+            if (alpha > 20) {
+              hitboxes[id][`${x}-${y}`] = true;
             }
           }
-          backBufferCtx.drawImage(
-            img,
-            scaledLeft,
-            scaledTop,
-            scaledWidth,
-            scaledHeight
-          );
         }
-        resolve();
+        // }
+        resolve({ ...image, image: img });
       });
     });
   };
 
-  for (const img of images) {
-    await makeImg(img);
-  }
-  const ctx = canvas.getContext("2d");
-  ctx.imageSmoothingEnabled = false;
-  ctx.drawImage(backBuffer, 0, 0, canvas.width, canvas.height);
+  images = await Promise.all(images.map(makeImg));
 
-  const drawHitboxen = () => {
-    var imgData = ctx.createImageData(canvas.width, canvas.height); // only do this once per page
-    for (let i = 0; i < imgData.data.length; i += 4) {
-      imgData.data[i] = 0;
-      imgData.data[i + 1] = 0;
-      imgData.data[i + 2] = 0;
-      imgData.data[i + 3] = 0;
+  const makeCanvas = (image) => {
+    const canvas = document.createElement("canvas");
+    canvas.style =
+      "position: absolute; top:0; left:0; background: transparent;";
+    canvas.width = actualWidth;
+    canvas.height = actualHeight;
+    const ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+    canvasContainer.appendChild(canvas);
+    return { ...image, canvas, ctx };
+  };
+
+  images = images.map(makeCanvas);
+
+  const drawImg = (img, { tint = null, clear = false } = {}) => {
+    const { ctx, image, left, top, width, height } = img;
+    if (clear) {
+      ctx.clearRect(0, 0, actualWidth, actualHeight);
+    } else if (tint) {
+      const { color, alpha = 0.5 } = tint;
+      const backBuffer = document.createElement("canvas");
+      backBuffer.width = actualWidth;
+      backBuffer.height = actualHeight;
+      const btx = backBuffer.getContext("2d");
+      btx.clearRect(0, 0, actualWidth, actualHeight);
+      btx.drawImage(image, left, top, width, height);
+
+      // Now we'll multiply a rectangle of your chosen color
+      btx.fillStyle = color;
+      btx.globalCompositeOperation = "multiply";
+      btx.fillRect(0, 0, actualWidth, actualHeight);
+
+      btx.globalAlpha = alpha;
+      btx.globalCompositeOperation = "destination-in";
+      btx.drawImage(image, left, top, width, height);
+      ctx.drawImage(backBuffer, 0, 0, actualWidth, actualHeight);
+    } else {
+      ctx.drawImage(image, left, top, width, height);
     }
-    for (const [id, vals] of Object.entries(hitboxes)) {
-      for (const xy in vals) {
-        const [x, y] = xy.split("-");
-        const i =
-          (canvas.height - 1 - parseInt(y)) * canvas.width + parseInt(x);
+  };
 
-        imgData.data[i * 4] = 255;
-        imgData.data[i * 4 + 3] = 255;
+  images.forEach((i) => drawImg(i));
+
+  const state = { cavity: null, selected: null };
+
+  canvasContainer.addEventListener("click", (e) => {
+    const canvasRect = canvasContainer.getBoundingClientRect();
+    const x = Math.floor(e.clientX - canvasRect.left);
+    const y = Math.floor(e.clientY - canvasRect.top);
+
+    for (const [hitboxId, vals] of Object.entries(hitboxes)) {
+      if (vals[`${x}-${y}`]) {
+        const image = images.find(({ id }) => id === hitboxId);
+        if (image) {
+          const target = parseInt(hitboxId.split("-")[1], 10);
+          if (state.cavity === null) {
+            drawImg(image, { tint: { color: "#F00000", alpha: 0.3 } });
+            state.cavity = target;
+            break;
+          } else if (
+            target < state.cavity &&
+            (state.selected == null || state.selected > target)
+          ) {
+            // Hide all teeth up to this one
+            for (let i = 1; i <= target; i++) {
+              debugger;
+              const image = images.find(({ id }) => id === `tooth-${i}`);
+              console.log(image);
+              if (image) {
+                drawImg(image, { clear: true });
+              }
+            }
+            state.step = "done";
+          }
+          break;
+        }
       }
     }
-    // TODO: This wipes the canvas
-    ctx.putImageData(imgData, 0, 0);
-  };
-  // Draw the polys (for debugging)
-  // drawHitboxen();
+  });
+  // Show the solution when you click teeth (after cavity)
+  //    a) if you're in a winnning position
+  //    b) if not then play 1
+  // Add a reset button
+  // Add some explanatory text: state-dependent
 };
 
-drawFrame();
-// TODO: Scan each tooth image and identify opaque pixels
-//      (rather than use a fiddly polygon)
-// Add a reset button
-// Click cavity
-// Tint cavity image red
-// Click current thingy and hide teeth
-// Show the solution
+main();
